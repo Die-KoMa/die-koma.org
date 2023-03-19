9efe041fc66f369ae9377fdb2f779d2c0563ca79
Jan Beinke <git@janbeinke.com>
2023-03-19 16:57:33 +0100

    Convert nix build to a proper package

--- nix/homepage.nix
+++ nix/homepage.nix
@@ -1,18 +1,24 @@
 { lib
-, runCommandNoCC
+, stdenvNoCC
 , jekyll
 , doTar ? false
 , gnutar
 }:
 with lib;
 
-let
-  jekyllOutdir = if doTar then "build" else "$out";
-  tarEnding = ".tar.xz";
-in
-runCommandNoCC "die-koma.org${optionalString doTar tarEnding}" {} ''
-  cp -r --reflink=auto --no-preserve=mode ${../.}/* ./
-  rm -rf Dockerfile env-vars flake.lock flake.nix Makefile nix README.md
-  ${jekyll}/bin/jekyll build --destination=${jekyllOutdir}
-  ${optionalString doTar "${gnutar}/bin/tar -cvf $out --xz build"}
-''
+stdenvNoCC.mkDerivation {
+  name = "die-koma.org${optionalString doTar ".tar.xz"}";
+  src = ../.;
+
+  nativeBuildInputs = [ jekyll ];
+
+  buildPhase = ''
+    rm -rf Dockerfile env-vars flake.lock flake.nix Makefile nix README.md
+    jekyll build --destination=build
+  '';
+
+  installPhase =
+    if doTar
+    then "tar -cvf $out --xz build"
+    else "cp -r build $out";
+}
