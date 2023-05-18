34556c88b37114d028e652e2d3eefb36239c4fcf
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2023-05-18 14:58:19 +0200

    Add action to check for broken links

--- flake.nix
+++ flake.nix
@@ -2,31 +2,56 @@
 
   inputs = {
 
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     wat = {
-      url = github:thelegy/wat;
+      url = "github:thelegy/wat";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
   };
 
-  outputs = { self, nixpkgs, wat }: {
+  outputs = { self, nixpkgs, wat }:
+    let
+      withPackages = wat.lib.withPkgsFor [ "x86_64-linux" ] nixpkgs
+        [ self.overlays.default ];
+    in {
+      overlays.default = final: prev: {
+        KoMaHomepage = final.callPackage ./nix/homepage.nix { };
+        KoMaHomepageTar =
+          final.callPackage ./nix/homepage.nix { doTar = true; };
+      };
 
-    overlays.default = final: prev: {
-      KoMaHomepage = final.callPackage ./nix/homepage.nix {};
-      KoMaHomepageTar = final.callPackage ./nix/homepage.nix { doTar = true; };
+      packages = withPackages (pkgs: {
+
+        KoMaHomepage = pkgs.KoMaHomepage;
+        KoMaHomepageTar = pkgs.KoMaHomepageTar;
+
+        default = pkgs.KoMaHomepage;
+
+      });
+
+      apps = withPackages (pkgs:
+        let
+          checkLinks = pkgs.writeShellScript "check-links" ''
+            LANG="C.UTF-8" ${pkgs.html-proofer}/bin/htmlproofer --allow-hash-href --assume-extension --empty-alt-ignore --http-status-ignore 401 ${pkgs.KoMaHomepage}
+          '';
+          serve = pkgs.writeShellScript "serve" ''
+            ${pkgs.jekyll}/bin/jekyll serve
+          '';
+        in rec {
+          check-links = {
+            type = "app";
+            program = "${checkLinks}";
+          };
+
+          serve = {
+            type = "app";
+            program = "${serve}";
+          };
+
+          default = serve;
+        });
     };
-
-    packages = wat.lib.withPkgsFor [ "x86_64-linux" ] nixpkgs [ self.overlays.default ] (pkgs: {
-
-      KoMaHomepage = pkgs.KoMaHomepage;
-      KoMaHomepageTar = pkgs.KoMaHomepageTar;
-
-      default = pkgs.KoMaHomepage;
-
-    });
-
-  };
 
 }
