e3bea3f278653ff3769a315e12ad2e4721fb6aa7
Jan Beinke <git@janbeinke.com>
2024-01-30 02:16:44 +0100

    Remove astro-compress as it leads to build issues

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
 
-import compress from 'astro-compress'
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), compress()],
+  integrations: [tailwind(), react()],
 })
