389129100d2db0080ff51cd2b715c916b7135149
Jan Beinke <git@janbeinke.com>
2024-02-03 01:42:09 +0100

    Add flake apps to build the new website and host a dev server

--- astro/flake.nix
+++ astro/flake.nix
@@ -1,12 +1,38 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
-  outputs = {nixpkgs, ...}: let
+  outputs = flakes @ {nixpkgs, ...}: let
     overlays = [
+      flakes.self.overlays.default
     ];
     withPkgsFor = systems: fn: with nixpkgs.lib; genAttrs systems (system: fn (import nixpkgs {inherit system overlays;}));
     withPkgs = withPkgsFor nixpkgs.lib.platforms.unix;
   in {
+    overlays.default = final: prev: {
+      koma-homepage-build = {
+        type = "app";
+        program = let
+          build = final.writeShellScript "build" ''
+            export PATH=${final.nodePackages.nodejs}/bin
+            ${final.bun}/bin/bun run build
+          '';
+        in "${build}";
+      };
+      koma-homepage-dev = {
+        type = "app";
+        program = let
+          build = final.writeShellScript "build" ''
+            export PATH=${final.nodePackages.nodejs}/bin
+            ${final.bun}/bin/bun run dev "$@"
+          '';
+        in "${build}";
+      };
+    };
+    apps = withPkgs (pkgs: {
+      build = pkgs.koma-homepage-build;
+      dev = pkgs.koma-homepage-dev;
+      default = pkgs.koma-homepage-dev;
+    });
     devShells = withPkgs (pkgs: {
       default = pkgs.mkShell {
         name = "koma-homepage-shell";
