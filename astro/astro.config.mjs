42f87376c9d4f0e5c6ddc57beb73be52ffd72cca
Jan Beinke <git@janbeinke.com>
2024-02-03 01:23:21 +0100

    Integrate astro-compress

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
 
+import compress from 'astro-compress'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), compress()],
 })
