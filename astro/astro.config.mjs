b3b31c8296fa4a7de038a508aef22a2a3ddae923
Jan Beinke <git@janbeinke.com>
2024-02-11 05:27:28 +0100

    Add support for mdx

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
 
 import compress from 'astro-compress'
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress()],
+  integrations: [mdx(), tailwind(), compress()],
 })
