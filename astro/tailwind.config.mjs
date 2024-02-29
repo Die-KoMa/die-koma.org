88110dd4d33931d4147ae11b0da758ea9bfa488a
Jan Beinke <git@janbeinke.com>
2024-02-29 22:50:08 +0100

    Fix mutiple issues in build system and add code for external links

--- astro/tailwind.config.mjs
+++ astro/tailwind.config.mjs
@@ -1,9 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+
+import plugin from 'tailwindcss/plugin'
+
+const tailwindPlugin = plugin(({ theme, matchUtilities }) => {
+  matchUtilities(
+    {
+      mask: (value) => ({
+        maskImage: value,
+      }),
+    },
+    {
+      values: {
+        ...theme('backgroundImage'),
+        none: 'none',
+      },
+    },
+  )
+})
+
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./astro.config.*', './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        externalLink:
+          "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-6 h-6'%3E%3Cpath stroke-linecap='round' stroke-linejoin='round' d='M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25' /%3E%3C/svg%3E\")",
+      },
+    },
   },
   darkMode: ['variant', ['@media(prefers-color-scheme: dark) {.no-theme &}', '.dark &']],
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/typography'), tailwindPlugin],
 }
