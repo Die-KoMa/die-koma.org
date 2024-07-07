f742bc8e6f0353fb8a6a8e28db7cc3f55ab98a82
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2024-07-07 17:03:17 +0200

    Drop superfluous extlink icon in next KoMa box

--- flake.nix
+++ flake.nix
@@ -1,45 +1,57 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
-  outputs = flakes@{ nixpkgs, ... }:
+  outputs =
+    flakes@{ nixpkgs, ... }:
     let
       overlays = [ flakes.self.overlays.default ];
-      withPkgsFor = systems: fn:
+      withPkgsFor =
+        systems: fn:
         with nixpkgs.lib;
-        genAttrs systems
-        (system: fn (import nixpkgs { inherit system overlays; }));
+        genAttrs systems (system: fn (import nixpkgs { inherit system overlays; }));
       withPkgs = withPkgsFor nixpkgs.lib.platforms.unix;
-    in {
-      overlays.default = final: prev:
+    in
+    {
+      overlays.default =
+        final: prev:
         let
           runNpm = args: {
             type = "app";
-            program = let
-              build = final.writeShellScript "build" ''
-                export PATH=${final.bash}/bin:${final.nodePackages.nodejs}/bin:${final.nodePackages.npm}/bin
-                npm ${args}
-              '';
-            in "${build}";
+            program =
+              let
+                build = final.writeShellScript "build" ''
+                  export PATH=${final.bash}/bin:${final.nodePackages.nodejs}/bin:${final.nodePackages.npm}/bin
+                  npm ${args}
+                '';
+              in
+              "${build}";
           };
-        in {
+        in
+        {
           koma-homepage = final.callPackage ./default.nix { };
           koma-homepage-tar = final.koma-homepage.override { doTar = true; };
           koma-homepage-install = runNpm ''install "$@"'';
           koma-homepage-build = runNpm "run build";
           koma-homepage-dev = runNpm ''run dev "$@"'';
+          koma-homepage-audit = runNpm ''audit "$@"'';
+          koma-homepage-update = runNpm ''update "$@"'';
         };
       apps = withPkgs (pkgs: {
         build = pkgs.koma-homepage-build;
         install = pkgs.koma-homepage-install;
         dev = pkgs.koma-homepage-dev;
         default = pkgs.koma-homepage-dev;
+        update = pkgs.koma-homepage-update;
+        audit = pkgs.koma-homepage-audit;
         check-links = {
           type = "app";
-          program = let
-            checkLinks = pkgs.writeShellScript "check-links" ''
-              LANG="C.UTF-8" ${pkgs.html-proofer}/bin/htmlproofer --allow-hash-href --assume-extension --empty-alt-ignore --ignore-status-codes 401 ${pkgs.koma-homepage}
-            '';
-          in "${checkLinks}";
+          program =
+            let
+              checkLinks = pkgs.writeShellScript "check-links" ''
+                LANG="C.UTF-8" ${pkgs.html-proofer}/bin/htmlproofer --allow-hash-href --assume-extension --empty-alt-ignore --ignore-status-codes 401 ${pkgs.koma-homepage}
+              '';
+            in
+            "${checkLinks}";
         };
       });
       packages = withPkgs (pkgs: {
@@ -51,8 +63,11 @@
       devShells = withPkgs (pkgs: {
         default = pkgs.mkShell {
           name = "koma-homepage-shell";
-          buildInputs =
-            [ pkgs.nodePackages.nodejs pkgs.nodePackages.npm pkgs.tailwindcss ];
+          buildInputs = [
+            pkgs.nodePackages.nodejs
+            pkgs.nodePackages.npm
+            pkgs.tailwindcss
+          ];
         };
       });
     };
