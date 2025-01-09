1b583ed843b9687b73e4dad0d05864734d0e8a9f
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2025-01-09 12:33:15 +0100

    Prefer copy over symlink for package{,-lock}.json
    For whichever reason, using symlinks breaks with nix 2.19+, while
    building fine on nix 2.18.

--- default.nix
+++ default.nix
@@ -9,42 +9,45 @@
   vips,
   pkg-config,
 }:
-with lib; let
+with lib;
+let
   srcN2N =
-    runCommand "koma-homepage-node2nix" {
-      buildInputs = [node2nix];
-    } ''
-      mkdir $out
+    runCommand "koma-homepage-node2nix"
+      {
+        buildInputs = [ node2nix ];
+      }
+      ''
+        mkdir $out
 
-      ln -s ${./package.json} $out/package.json
-      ln -s ${./package-lock.json} $out/package-lock.json
+        cp ${./package.json} $out/package.json
+        cp ${./package-lock.json} $out/package-lock.json
 
-      cd $out
-      node2nix --lock package-lock.json --development
-    '';
+        cd $out
+        node2nix --lock package-lock.json --development
+      '';
 
-  resultN2N = callPackage (import srcN2N) {};
+  resultN2N = callPackage (import srcN2N) { };
 
   nodeDependencies = resultN2N.nodeDependencies.overrideAttrs (oldAttrs: {
-    buildInputs = oldAttrs.buildInputs ++ [pkg-config vips.dev];
+    buildInputs = oldAttrs.buildInputs ++ [
+      pkg-config
+      vips.dev
+    ];
   });
 in
-  stdenv.mkDerivation {
-    name = "die-koma.org${optionalString doTar ".tar.xz"}";
-    src = ./.;
-    buildInputs = [nodejs];
-    buildPhase = ''
-      mkdir node_modules
-      ln -s ${nodeDependencies}/lib/node_modules/* ./node_modules
-      export PATH="${nodeDependencies}/bin:$PATH"
+stdenv.mkDerivation {
+  name = "die-koma.org${optionalString doTar ".tar.xz"}";
+  src = ./.;
+  buildInputs = [ nodejs ];
+  buildPhase = ''
+    mkdir node_modules
+    ln -s ${nodeDependencies}/lib/node_modules/* ./node_modules
+    export PATH="${nodeDependencies}/bin:$PATH"
 
-      ln -sf ${./next-koma.json} next-koma.json
+    ln -sf ${./next-koma.json} next-koma.json
 
-      mkdir config-home
-      XDG_CONFIG_HOME=config-home npm run build
-    '';
-    installPhase =
-      if doTar
-      then "tar -cvf $out --xz dist"
-      else "cp -r dist $out";
-  }
+    mkdir config-home
+    XDG_CONFIG_HOME=config-home npm run build
+  '';
+  installPhase = if doTar then "tar -cvf $out --xz dist" else "cp -r dist $out";
+}
